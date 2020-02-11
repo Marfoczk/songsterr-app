c1448a033def4b863f287ef8f5a256893478a01f
Marcin Głażewski <marfoczok@gmail.com>
2020-02-11 18:58:34 +0100

    Updates

---
+++ precache-manifest.f882a87aa77ef2057a96009a01df724c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d2cfa3c9c9734928bc463b04f48ade",
+    "url": "/songsterr-app/index.html"
+  },
+  {
+    "revision": "00f725f9a6a8ff7c8f62",
+    "url": "/songsterr-app/static/css/main.663485eb.chunk.css"
+  },
+  {
+    "revision": "467e6874439f10c86966",
+    "url": "/songsterr-app/static/js/2.4e076a8a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/songsterr-app/static/js/2.4e076a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f725f9a6a8ff7c8f62",
+    "url": "/songsterr-app/static/js/main.643df426.chunk.js"
+  },
+  {
+    "revision": "f197da3cd50c984b7d73",
+    "url": "/songsterr-app/static/js/runtime-main.b0268f1b.js"
+  }
+]);
